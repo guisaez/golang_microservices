a4bcb2b976eccd492387de58de8f25fb20bd0c5a
guisaez <guillesaez15@icloud.com>
2024-03-23 18:28:27 -0400

    Modifying CORS Policy

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -19,7 +19,7 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 
 	out, _ := json.MarshalIndent(payload, "", "\t")
 
-	w.Header().Set("coNTENT-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusAccepted)
 	w.Write(out)
 }
--- broker-service/cmd/api/routes.go
+++ broker-service/cmd/api/routes.go
@@ -12,7 +12,7 @@ func (app *Config) routes() http.Handler {
 
 	// specify who is allowed to connect
 	mux.Use(cors.Handler(cors.Options{
-		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedOrigins: []string{"*"},
 		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders: []string{"Link"},
