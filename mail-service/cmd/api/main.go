73a607f02e98003a77c95eee88cd5883560ce8e4
guisaez <guillesaez15@icloud.com>
2024-03-29 22:56:30 -0400

    Mail Serivce Done

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -4,21 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 type Config struct {
-	
+	Mailer Mail
 }
 
 const webPort = "80"
 
 func main() {
-	app := Config{}
+	app := Config{
+		Mailer: createMail(),
+	}
 
 	log.Println("Starting mail service on port", webPort)
 
 	srv := &http.Server{
-		Addr: fmt.Sprintf(":%s", webPort),
+		Addr:    fmt.Sprintf(":%s", webPort),
 		Handler: app.routes(),
 	}
 
@@ -26,4 +30,20 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+func createMail() Mail {
+	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	m := Mail{
+		Domain:          os.Getenv("MAIL_DOMAIN"),
+		Host:            os.Getenv("MAIL_HOST"),
+		Port:            port,
+		Username:        os.Getenv("MAIL_USERNAME"),
+		Password:        os.Getenv("MAIL_PASSWORD"),
+		Encryption:      os.Getenv("MAIL_ENCRYPTION"),
+		FromAddress:     os.Getenv("FROM_NAME"),
+		DefaultFromName: os.Getenv("FROM_ADDRESS"),
+	}
+
+	return m
 }
---
+++ mail-service/mail-service.dockerfile
@@ -0,0 +1,22 @@
+FROM golang:1.22 as builder
+
+RUN mkdir /app
+
+COPY . /app
+
+WORKDIR /app
+
+RUN CGO_ENABLED=0 go build -o mailService ./cmd/web
+
+RUN chmod +x /app/mailService
+
+FROM alpine:latest
+
+RUN mkdir /app
+
+WORKDIR /app
+
+COPY --from=builder /app/templates /app/templates
+COPY --from=builder /app/mailService /app/mailService
+
+CMD ["/app/frontEnd"]
